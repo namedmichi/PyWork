ceccf5b683ac2dd6a809b042267e76ee9f5f14d7
namedmichi <j.technology@web.de>
2023-02-20 15:36:50 +0100

    sync

---
+++ umlaute.py
@@ -0,0 +1,8 @@
+text = input("geben sie einen Text ein: ")
+counter = 0
+umlaute = {"a", "e", "i", "o", "u"}
+
+for item in text:
+    if item in umlaute:
+        counter = counter + 1
+print(counter)
