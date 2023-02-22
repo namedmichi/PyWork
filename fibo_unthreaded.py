9991ad76f37e220c6afbb4f454bf6af03b2f84bf
namedmichi <j.technology@web.de>
2023-02-22 20:40:32 +0100

    push

---
+++ fibo_unthreaded.py
@@ -0,0 +1,13 @@
+fiboNum = [1, 1]
+
+
+def fibo(n):
+    i = 0
+    while i < n:
+        ergebniss = fiboNum[i] + fiboNum[i+1]
+        i = i + 1
+        fiboNum.append(ergebniss)
+    return fiboNum[n]
+
+
+print(fibo(82346))
