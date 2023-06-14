dec21e11fcfb6b69360e10295617055e7db23545
namedmichi <j.technology@web.de>
2023-06-14 21:53:18 +0200

    push

---
+++ rechner2.py
@@ -0,0 +1,6 @@
+def rechner(operator):
+    zahl1 = input("Gib eine Zahl ein: ")
+    zahl2 = input("Gib eine weitere Zahl ein: ")
+    string = zahl1 + operator + zahl2
+    return eval(string)
+print(rechner(input("Gib einen Operator ein: ")))
---
+++ rechner3.py
@@ -0,0 +1,3 @@
+def rechner(operator):
+    return eval(input("Gib eine Zahl ein: ") + operator + input("Gib eine weitere Zahl ein: "))
+print(rechner(input("Gib einen Operator ein: ")))
